f2b505f34e6f45ee6b76a58155ba7674a34b181e
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-22 17:54:33 -0300

    feat: create Expiry type

---
+++ src/typedefs/Expiry.ts
@@ -0,0 +1,49 @@
+export type Expiry =
+    | "never"
+    | "NEVER"
+    | "n"
+    | "N"
+    | "10 minutes"
+    | "10 MINUTES"
+    | "10m"
+    | "10M"
+    | "1 hour"
+    | "1 HOUR"
+    | "1h"
+    | "1H"
+    | "1 day"
+    | "1 DAY"
+    | "1d"
+    | "1D"
+    | "1 week"
+    | "1 WEEK"
+    | "1w"
+    | "1W"
+    | "2 weeks"
+    | "2 WEEKS"
+    | "2w"
+    | "2W"
+    | "1 month"
+    | "1 MONTH"
+    | "1m"
+    | "1M"
+    | "6 months"
+    | "6 MONTHS"
+    | "6m"
+    | "6M"
+    | "1 year"
+    | "1 YEAR"
+    | "1y"
+    | "1Y"
+
+export function resolveExpiry(
+    input: string
+): "N" | "10M" | "1H" | "1D" | "1W" | "2W" | "1M" | "6M" | "1Y" {
+    const initials = input
+        .split(" ")
+        .map(w => w[0])
+        .join("")
+    return <"N" | "10M" | "1H" | "1D" | "1W" | "2W" | "1M" | "6M" | "1Y">(
+        initials.toUpperCase()
+    )
+}
