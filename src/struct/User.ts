c134f3bc69f174a1ae1c2da847deafd7f5fb281d
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-22 16:53:52 -0300

    refactor(User): move to typescript

---
+++ src/struct/User.ts
@@ -0,0 +1,31 @@
+import PastebinClient from "./PastebinClient"
+
+/**
+ * A Pastebin user.
+ */
+export default class User {
+    /**
+     * The client used to get this user.
+     */
+    client: PastebinClient
+    /**
+     * This user's username.
+     */
+    username: string
+
+    /**
+     * @param client The client used to get the user
+     * @param username The user's username
+     */
+    constructor(client: PastebinClient, username: string) {
+        this.client = client
+        this.username = username
+    }
+
+    /**
+     * Whether this user is the same as the client's user.
+     */
+    get me(): boolean {
+        return this.username === this.client.username
+    }
+}
