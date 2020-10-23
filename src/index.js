f960b97c84b6f2f16f9a79b31981884a60350d34
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-22 23:29:22 -0300

    refactor(index): move to typescript

--- src/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.PastebinClient = require("./struct/PastebinClient")
-module.exports.PastebinError = require("./struct/PastebinError")
-module.exports.Paste = require("./struct/Paste")
-module.exports.User = require("./struct/User")
-module.exports.PasteStore = require("./struct/stores/PasteStore")
-module.exports.UserStore = require("./struct/stores/UserStore")
-module.exports.UserPasteStore = require("./struct/stores/UserPasteStore")
